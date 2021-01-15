0444065269f602200eb3e9c8a9306d02e6f2b681
jerryzz668 <jerryzz668@163.com>
2021-01-15 10:12:29 +0800

    dic2xml

--- preprocessing/dic2xml.py
+++ preprocessing/dic2xml.py
@@ -7,89 +7,90 @@ import time
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
 
-def create_Node(element, text=None):
-    elem = ET.Element(element)
-    elem.text = text
-    return elem
+class Dic2Xml():
+    def create_Node(self, element, text=None):
+        elem = ET.Element(element)
+        elem.text = text
+        return elem
 
-def link_Node(root, element, text=None):
-    """
-    @param root: element的父节点
-    @param element: 创建的element子节点
-    @param text: element节点内容
-    @return: 创建的子节点
-    """
-    if text != None:
-        text = str(text)
-    element = create_Node(element, text)
-    root.append(element)
-    return element
+    def link_Node(self, root, element, text=None):
+        """
+        @param root: element的父节点
+        @param element: 创建的element子节点
+        @param text: element节点内容
+        @return: 创建的子节点
+        """
+        if text != None:
+            text = str(text)
+        element = self.create_Node(element, text)
+        root.append(element)
+        return element
 
-# 保存为XML文件（美化后）
-def saveXML(root, filename, indent="\t", newl="\n", encoding="utf-8"):
-    rawText = ET.tostring(root)
-    dom = minidom.parseString(rawText)
-    with open(filename, 'w') as f:
-        dom.writexml(f, "", indent, newl, encoding)
+    # 保存为XML文件（美化后）
+    def saveXML(self, root, filename, indent="\t", newl="\n", encoding="utf-8"):
+        rawText = ET.tostring(root)
+        dom = minidom.parseString(rawText)
+        with open(filename, 'w') as f:
+            dom.writexml(f, "", indent, newl, encoding)
 
-def get_dic_data(key, value):
-    save_name = key.split('.')[0]+'.xml'
-    anno = value.get('anno')
-    w = value.get('w')
-    h = value.get('h')
-    return save_name, save_name, anno, None, 'true', w, h, 3
+    def get_dic_data(self, key, value):
+        save_name = key.split('.')[0]+'.xml'
+        anno = value.get('anno')
+        w = value.get('w')
+        h = value.get('h')
+        return save_name, save_name, anno, None, 'true', w, h, 3
 
-def generate_xml(key, value, xml_save_path):
-    save_name, xmlpath, anno, time_label, image_label, width, height, depth = get_dic_data(key, value)
+    def generate_xml(self, key, value, xml_save_path):
+        save_name, xmlpath, anno, time_label, image_label, width, height, depth = self.get_dic_data(key, value)
 
-    root = ET.Element("doc")  # 创建根结点
+        root = ET.Element("doc")  # 创建根结点
 
-    path = link_Node(root, 'path', xmlpath)  # 创建path节点
-    outputs = link_Node(root, 'outputs')
-    object = link_Node(outputs, 'object')
+        path = self.link_Node(root, 'path', xmlpath)  # 创建path节点
+        outputs = self.link_Node(root, 'outputs')
+        object = self.link_Node(outputs, 'object')
 
-    for i in range(len(anno)):
-        item = link_Node(object, 'item')  # 创建item节点
+        for i in range(len(anno)):
+            item = self.link_Node(object, 'item')  # 创建item节点
 
-        label = anno[i][4]  # 获取label
-        width_points_line = 2  # 点或线的width
-        shape_type = 'rectangle'
+            label = anno[i][4]  # 获取label
+            width_points_line = 2  # 点或线的width
+            shape_type = 'rectangle'
 
-        name = link_Node(item, 'name', label)  # 添加json信息到item中
-        width_2 = link_Node(item, 'width', width_points_line)
+            name = self.link_Node(item, 'name', label)  # 添加json信息到item中
+            width_2 = self.link_Node(item, 'width', width_points_line)
 
-        if shape_type == 'rectangle':
-            bndbox = link_Node(item, 'bndbox')
-            xmin = link_Node(bndbox, 'xmin', int(anno[i][0]))
-            ymin = link_Node(bndbox, 'ymin', int(anno[i][1]))
-            xmax = link_Node(bndbox, 'xmax', int(anno[i][2]))
-            ymax = link_Node(bndbox, 'ymax', int(anno[i][3]))
+            if shape_type == 'rectangle':
+                bndbox = self.link_Node(item, 'bndbox')
+                xmin = self.link_Node(bndbox, 'xmin', int(anno[i][0]))
+                ymin = self.link_Node(bndbox, 'ymin', int(anno[i][1]))
+                xmax = self.link_Node(bndbox, 'xmax', int(anno[i][2]))
+                ymax = self.link_Node(bndbox, 'ymax', int(anno[i][3]))
 
-        status = link_Node(item, 'status', str(0))
+            status = self.link_Node(item, 'status', str(0))
 
-    time_labeled = link_Node(root, 'time_labeled', time_label)  # 创建time_labeled节点
-    labeled = link_Node(root, 'labeled', image_label)
-    size = link_Node(root, 'size')
-    width = link_Node(size, 'width', width)
-    height = link_Node(size, 'height', height)
-    depth = link_Node(size, 'depth', depth)
+        time_labeled = self.link_Node(root, 'time_labeled', time_label)  # 创建time_labeled节点
+        labeled = self.link_Node(root, 'labeled', image_label)
+        size = self.link_Node(root, 'size')
+        width = self.link_Node(size, 'width', width)
+        height = self.link_Node(size, 'height', height)
+        depth = self.link_Node(size, 'depth', depth)
 
-    save_path = os.path.join(xml_save_path, save_name)
-    if not os.path.exists(xml_save_path):
-        os.makedirs(xml_save_path)
-    # 保存xml文件
-    saveXML(root, save_path)
-    print('{}'.format(save_name) + ' has been transformed!')
+        save_path = os.path.join(xml_save_path, save_name)
+        if not os.path.exists(xml_save_path):
+            os.makedirs(xml_save_path)
+        # 保存xml文件
+        self.saveXML(root, save_path)
+        print('{}'.format(save_name) + ' has been transformed!')
 
-def dic2xml(dic, xml_save_path):
-    t = time.time()
-    for key, value in dic.items():
-        generate_xml(key, value, xml_save_path)
-    print(time.time()-t)
+    def dic2xml(self, dic, xml_save_path):
+        t = time.time()
+        for key, value in dic.items():
+            self.generate_xml(key, value, xml_save_path)
+        print(time.time()-t)
 
 if __name__ == '__main__':
     dic = {'123.jpg': {'anno': [(132, 243, 355, 467, '刮伤'), (51, 61, 72, 82, '异色')], 'w': 512, 'h': 512},
            '456.jpg': {'anno': [(11, 21, 31, 41, '擦伤'), (11, 22, 33, 41, '白点')], 'w': 512, 'h': 512}}
 
     xml_save_path = '/Users/zhangyan/Desktop/xml'
-    dic2xml(dic, xml_save_path)
+    Dic2Xml().dic2xml(dic, xml_save_path)
--- preprocessing/test.py
+++ preprocessing/test.py
@@ -3,120 +3,22 @@
 # @Time       : 2020/12/30 2:20 下午
 
 
-# class Test:
-#     def __init__(self, a, b, c):
-#         self.a = a
-#         self.b = b
-#         self.c = c
-#         self.plus(a, b)
-#         self.minus(b, c)
-#     def plus(self, f, g):
-#         print(f+g)
-#     def minus(self, d, e):
-#         print(d-e)
-#
-# # Test(1,2,3)
-#
-# for i in range(10):
-#     if i > 5:
-#         print(i)
-#         continue
-#     print('111')
+class Test:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.plus(a, b)
+        self.minus(b, c)
+    def plus(self, f, g):
+        print(f+g)
+    def minus(self, d, e):
+        print(d-e)
 
-import os
-import time
-import xml.etree.ElementTree as ET
-from xml.dom import minidom
+# Test(1,2,3)
 
-
-class Dic2Xml():
-    def __init__(self, dic, xml_save_path):
-        self.dic2xml(dic, xml_save_path)
-
-    def create_Node(self, element, text=None):
-        elem = ET.Element(element)
-        elem.text = text
-        return elem
-
-    def link_Node(self, root, element, text=None):
-        """
-        @param root: element的父节点
-        @param element: 创建的element子节点
-        @param text: element节点内容
-        @return: 创建的子节点
-        """
-        if text != None:
-            text = str(text)
-        element = self.create_Node(element, text)
-        root.append(element)
-        return element
-
-    # 保存为XML文件（美化后）
-    def saveXML(root, filename, indent="\t", newl="\n", encoding="utf-8"):
-        rawText = ET.tostring(root)
-        dom = minidom.parseString(rawText)
-        with open(filename, 'w') as f:
-            dom.writexml(f, "", indent, newl, encoding)
-
-    def get_dic_data(self, key, value):
-        save_name = key.split('.')[0]+'.xml'
-        anno = value.get('anno')
-        w = value.get('w')
-        h = value.get('h')
-        return save_name, save_name, anno, None, 'true', w, h, 3
-
-    def generate_xml(self, key, value, xml_save_path):
-        save_name, xmlpath, anno, time_label, image_label, width, height, depth = self.get_dic_data(key, value)
-
-        root = ET.Element("doc")  # 创建根结点
-
-        path = self.link_Node(root, 'path', xmlpath)  # 创建path节点
-        outputs = self.link_Node(root, 'outputs')
-        object = self.link_Node(outputs, 'object')
-
-        for i in range(len(anno)):
-            item = self.link_Node(object, 'item')  # 创建item节点
-
-            label = anno[i][4]  # 获取label
-            width_points_line = 2  # 点或线的width
-            shape_type = 'rectangle'
-
-            name = self.link_Node(item, 'name', label)  # 添加json信息到item中
-            width_2 = self.link_Node(item, 'width', width_points_line)
-
-            if shape_type == 'rectangle':
-                bndbox = self.link_Node(item, 'bndbox')
-                xmin = self.link_Node(bndbox, 'xmin', int(anno[i][0]))
-                ymin = self.link_Node(bndbox, 'ymin', int(anno[i][1]))
-                xmax = self.link_Node(bndbox, 'xmax', int(anno[i][2]))
-                ymax = self.link_Node(bndbox, 'ymax', int(anno[i][3]))
-
-            status = self.link_Node(item, 'status', str(0))
-
-        time_labeled = self.link_Node(root, 'time_labeled', time_label)  # 创建time_labeled节点
-        labeled = self.link_Node(root, 'labeled', image_label)
-        size = self.link_Node(root, 'size')
-        width = self.link_Node(size, 'width', width)
-        height = self.link_Node(size, 'height', height)
-        depth = self.link_Node(size, 'depth', depth)
-
-        save_path = os.path.join(xml_save_path, save_name)
-        if not os.path.exists(xml_save_path):
-            os.makedirs(xml_save_path)
-        # 保存xml文件
-        self.saveXML(root, save_path)
-        print('{}'.format(save_name) + ' has been transformed!')
-
-    def dic2xml(self, dic, xml_save_path):
-        t = time.time()
-        for key, value in dic.items():
-            self.generate_xml(key, value, xml_save_path)
-        print(time.time()-t)
-
-
-if __name__ == '__main__':
-    dic = {'123.jpg': {'anno': [(132, 243, 355, 467, '刮伤'), (51, 61, 72, 82, '异色')], 'w': 512, 'h': 512},
-           '456.jpg': {'anno': [(11, 21, 31, 41, '擦伤'), (11, 22, 33, 41, '白点')], 'w': 512, 'h': 512}}
-
-    xml_save_path = '/Users/zhangyan/Desktop/xml'
-    Dic2Xml(dic, xml_save_path)
+for i in range(10):
+    if i > 5:
+        print(i)
+        continue
+    print('111')
