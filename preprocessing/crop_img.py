8543942716e4dbb07f16ac8823df0a2abb0adc47
jerryzz668 <jerryzz668@163.com>
2021-03-02 17:46:47 +0800

    0302

---
+++ preprocessing/crop_img.py
@@ -0,0 +1,47 @@
+# @Description:
+# @Author     : zhangyan
+# @Time       : 2021/2/27 12:24 PM
+
+import os
+import time
+import cv2
+
+def crop_image(src, rownum, colnum, dstpath):
+    img = cv2.imread(src)
+    w, h = img.shape[1], img.shape[0]
+    if rownum <= h and colnum <= w:
+
+        s = os.path.split(src)
+        print('Original image info: %s, %sx%s' % (s[-1], w, h))
+        # print(s)
+        if dstpath == '':
+            dstpath = s[0]
+        fn = s[1].split('.')
+        basename = fn[0]
+        ext = fn[-1]
+
+        num = 0
+        rowheight = h // rownum
+        colwidth = w // colnum
+        for r in range(rownum):
+            for c in range(colnum):
+                cv2.imwrite(os.path.join(dstpath, basename + '_' + str(num) + '.' + ext), img[r * rowheight:(r + 1) * rowheight, c * colwidth:(c + 1) * colwidth])
+                num = num + 1
+    else:
+        print('不合法的行列切割参数！')
+
+def crop_images(img_path, rownum, colnum, dstpath):
+    if not os.path.exists(dstpath): os.mkdir(dstpath)
+    img_list = os.listdir(img_path)
+    t0 = time.time()
+    for img in img_list:
+        crop_image(os.path.join(img_path, img), rownum, colnum, dstpath)
+    t1 = time.time()
+    print('time:', t1-t0)
+
+
+
+if __name__ == '__main__':
+    img_path = '/Users/zhangyan/Desktop/1'  # img文件夹路径
+    save_path = '/Users/zhangyan/Desktop/crop_result'  # 保存路径
+    crop_images(img_path, 2, 2, save_path)
--- preprocessing/test.py
+++ preprocessing/test.py
@@ -9,32 +9,4 @@ import time
 import cv2
 import shutil
 
-a = random.randint(3,10)
-# print(a)
-#
-# print('\033[1;45m 字体不变色，有背景色 \033[0m')  # 有高亮
 
-save_path = '/Users/zhangyan/Desktop/img/'
-img_path = '/Users/zhangyan/Desktop/crop'
-# cemian = '/Users/zhangyan/Desktop/cemian'
-filelist = os.listdir(img_path)
-print(filelist)
-for file in filelist:
-    img = cv2.imread(os.path.join(img_path, file))
-    h,w = img.shape[0],img.shape[1]
-    if h ==8500 and w == 500:
-        shutil.copy(os.path.join(img_path, file), save_path)
-
-    # width = int(w/2)
-    # height = int(h/2)
-    # img1 = img[0:width, 0:height]
-    # img2 = img[width:w, 0:height]
-    # img3 = img[0:width, height:h]
-    # img4 = img[width:w, height:h]
-    # name = file.split('.')[0]
-    # cv2.imwrite(save_path+name+'_1.jpg', img1)
-    # cv2.imwrite(save_path+name+'_2.jpg', img2)
-    # cv2.imwrite(save_path+name+'_3.jpg', img3)
-    # cv2.imwrite(save_path+name+'_4.jpg', img4)
-
-    print(file + 'has been cropped!')
