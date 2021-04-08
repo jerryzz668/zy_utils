5986dcde66840c024110f01c25a43d1ce10c2a56
guochuan.zhang <jerryzz668@163.com>
2021-04-08 11:52:39 +0800

    csv_result_match

--- preprocessing/csv_result_match.py
+++ preprocessing/csv_result_match.py
@@ -12,13 +12,18 @@ from my_utils import mkdir
 
 
 def csv_to_val(csv_path):
-    pass
+    data = pd.read_csv(csv_path)
+    data = data.iloc[:, [1, 3, 4, 5, 6, 7, 8, 9]]
+    data = np.array(data)
+    return data
 
 def val_match(val1, val2):
     pass
 
 
 if __name__ == '__main__':
-    val1 = csv_to_val(csv_path=None)
-    val2 = csv_to_val(csv_path=None)
-    val = val_match(val1, val2)
+    csv_path = r'C:\Users\Administrator\Desktop\test.csv'
+    val1 = csv_to_val(csv_path=csv_path)
+    print(val1)
+    # val2 = csv_to_val(csv_path=None)
+    # val = val_match(val1, val2)
