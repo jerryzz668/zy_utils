a8beb6ef7bde9d65c1a5408c1410435fd35a80c1
guochuan.zhang <jerryzz668@163.com>
2021-04-23 17:57:55 +0800

    test_to _report

--- preprocessing/test.py
+++ preprocessing/test.py
@@ -56,13 +56,26 @@ import os
 import openpyxl
 from openpyxl.styles import Font, Alignment
 
-font = Font(name='宋体', size=11, color='FF000000', bold=True, italic=False)
-align = Alignment(horizontal='center', vertical='center', wrap_text=False)
+
 
 excel_path = r'C:\Users\Administrator\Desktop\A.xlsx'
 
-book = xl.load_workbook(excel_path)
-sheet = book['ljq']
-sheet['A1'].font = font
-sheet['A1'].align = align
-book.save(excel_path)
+def beautify_excel(excel_path):
+    font = Font(name='宋体', size=11, color='FF000000', bold=True, italic=False)
+    align = Alignment(horizontal='center', vertical='center', wrap_text=False)
+
+    book = xl.load_workbook(excel_path)  # 加载excel
+    sheet_names = book.sheetnames  # 获取所有的sheet名称
+    sheet_names.remove('Sheet')
+
+    for sheet in sheet_names:
+        for row in book['{}'.format(sheet)]['A1:A5']:
+            for cell in row:
+                cell.font = font
+                cell.alignment = align
+
+        for row in book['{}'.format(sheet)]['A12:Z19']:
+            for cell in row:
+                cell.alignment = align
+
+    book.save(excel_path)
