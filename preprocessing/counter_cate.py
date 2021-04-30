b09f28bc65bd251ecbd3524e188feee87a376c98
guochuan.zhang <jerryzz668@163.com>
2021-04-30 11:20:19 +0800

    conunter_cate

--- preprocessing/counter_cate.py
+++ preprocessing/counter_cate.py
@@ -8,9 +8,8 @@ def parse_para(input_json):
     return ret_dic
 
 def main():
-    jsons = glob.glob(r'G:\weiruan_report\hongxing\gt\*.json')
+    jsons = glob.glob(r'G:\report\damian-v\img\jsons\*.json')
     dic = {}
-    gt_num =[]
     for i in jsons:
         ret_dic = parse_para(i)
         shapes = ret_dic['shapes']
@@ -19,13 +18,10 @@ def main():
                 dic[j['label']] = 1
             else:
                 dic[j['label']] += 1
-    a = sorted(dic.items(),key=lambda x:x[0],reverse=False)
+    a = sorted(dic.items(),key=lambda x:x[1],reverse=True)
     print(dic.keys())
     print(a)
-    for cls, num in a:
-        gt_num.append(num)
     print(len(a))
-    print(gt_num)
 
 if __name__ == '__main__':
     main()
