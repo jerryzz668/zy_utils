9604312131dd9e62014d782e0afdf3f2fc76f729
jerryzz668 <jerryzz668@163.com>
2021-10-07 21:17:01 +0800

    test

--- preprocessing/test.py
+++ preprocessing/test.py
@@ -2,6 +2,7 @@
 # @Author     : zhangyan
 # @Time       : 2020/12/30 2:20 下午
 
+from cv2 import PARAM_STRING
 from preprocessing.zy_utils import *
 import pandas as pd
 from tqdm import tqdm
@@ -39,25 +40,25 @@ from collections import Counter
 #     if instance['imageData'] != None:
 #         instance['imageData'] = None
 #     instance_to_json(instance, os.path.join(save_path, base_name))
-import cv2
-import matplotlib.pyplot as plt
-from atools.erode_dilated_img import MORPH_OPEN, MORPH_GRADIENT
+# import cv2
+# import matplotlib.pyplot as plt
+# from atools.erode_dilated_img import MORPH_OPEN, MORPH_GRADIENT
 
-input_dir = '/home/jerry/data/zAI_competition/modify/coco_aug/val2017'
-output_dir = '/home/jerry/data/zAI_competition/modify/coco_aug/val2017_r'
-make_dir(output_dir)
-img_list = glob.glob('{}/*.jpg'.format(input_dir))
-# img = '/home/jerry/data/zAI_competition/modify/coco_aug/val2017/connected_img_1.jpg'
-# print(img_list)
-for img in img_list:
-    print('processing:', img)
-    img_BGR = cv2.imread(img)
-    img_G = MORPH_OPEN(img_BGR)
-    img_B = MORPH_GRADIENT(img_BGR)
+# input_dir = '/home/jerry/data/zAI_competition/modify/coco_aug/val2017'
+# output_dir = '/home/jerry/data/zAI_competition/modify/coco_aug/val2017_r'
+# make_dir(output_dir)
+# img_list = glob.glob('{}/*.jpg'.format(input_dir))
+# # img = '/home/jerry/data/zAI_competition/modify/coco_aug/val2017/connected_img_1.jpg'
+# # print(img_list)
+# for img in img_list:
+#     print('processing:', img)
+#     img_BGR = cv2.imread(img)
+#     img_G = MORPH_OPEN(img_BGR)
+#     img_B = MORPH_GRADIENT(img_BGR)
 
-    img_BGG = np.concatenate([img_BGR[:, :, 2:3], img_B[:, :, 2:3], img_G[:, :, 2:3]], 2)
-    img_BGG = np.array(img_BGG, 'uint8')
-    cv2.imwrite(os.path.join(output_dir, os.path.basename(img)), img_BGG)
+#     img_BGG = np.concatenate([img_BGR[:, :, 2:3], img_B[:, :, 2:3], img_G[:, :, 2:3]], 2)
+#     img_BGG = np.array(img_BGG, 'uint8')
+#     cv2.imwrite(os.path.join(output_dir, os.path.basename(img)), img_BGG)
 
 # plt.subplot(221)
 # plt.imshow(img_BGR)
@@ -68,5 +69,27 @@ for img in img_list:
 # plt.subplot(224)
 # plt.imshow(img_BGG)
 # plt.show()
+
+import datetime
+ 
+def calc_spend_time(func):
+    def new_func(*args, **kargs):
+        start_time = datetime.datetime.now()
+        result = func(*args, **kargs)
+        end_tiem = datetime.datetime.now()
+        print ("result:", result, "used:", (end_tiem - start_time).microseconds, "μs")
+    return new_func
+ 
+@calc_spend_time
+def calc_add(a, b):
+    return a + b
+ 
+ 
+@calc_spend_time
+def calc_diff(a, b):
+    return a - b
+ 
+calc_add(a=1, b=2)
+calc_diff(1, 2)
 
 
