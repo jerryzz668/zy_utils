c621db422832bdc8dcd6f16c90493543d50bbd7b
jerryzz668 <jerryzz668@163.com>
2021-12-06 18:45:34 +0800

    bright

--- preprocessing/erode_dilated_img.py
+++ preprocessing/erode_dilated_img.py
@@ -53,6 +53,36 @@ def MORPH_BLACKHAT(image):
     dst = cv.morphologyEx(image, cv.MORPH_BLACKHAT, kernel)
     return dst
 
+# 图像亮度自适应增强(图像自动调亮)
+def compute(img, min_percentile, max_percentile):
+    """计算分位点，目的是去掉图1的直方图两头的异常情况"""
+    max_percentile_pixel = np.percentile(img, max_percentile)
+    min_percentile_pixel = np.percentile(img, min_percentile)
+    return max_percentile_pixel, min_percentile_pixel
+
+def get_lightness(src):
+    # 计算亮度
+    hsv_image = cv.cvtColor(src, cv.COLOR_BGR2HSV)
+    lightness = hsv_image[:, :, 2].mean()
+    return lightness
+
+def adaptive_brightness(src):
+    """图像亮度增强"""
+    if get_lightness(src) > 130:
+        print("图片亮度足够，不做增强")
+    # 先计算分位点，去掉像素值中少数异常值，这个分位点可以自己配置。
+    # 比如1中直方图的红色在0到255上都有值，但是实际上像素值主要在0到20内。
+    max_percentile_pixel, min_percentile_pixel = compute(src, 1, 99)
+
+    # 去掉分位值区间之外的值
+    src[src >= max_percentile_pixel] = max_percentile_pixel
+    src[src <= min_percentile_pixel] = min_percentile_pixel
+
+    # 将分位值区间拉伸到0到255，这里取了255*0.1与255*0.9是因为可能会出现像素值溢出的情况，所以最好不要设置为0到255。
+    out = np.zeros(src.shape, src.dtype)
+    cv.normalize(src, out, 255 * 0.1, 255 * 0.9, cv.NORM_MINMAX)
+    return out
+
 def modify_json_name(file, write_img_name, output_dir):
     json_basename = os.path.basename(file)
     json_dirname = os.path.dirname(file)
@@ -93,7 +123,7 @@ def erode_img(input_dir, filter, probability, rename):
             cv.imwrite(os.path.join(output_dir, '{}'.format(write_img_name) + img_name), processed_img, [int(cv.IMWRITE_JPEG_QUALITY), 95])
 
 if __name__ == '__main__':
-    input_dir = '/home/jerry/Desktop/erode_test'  # 输入img和json // or only img // or img和部分json
-    filter = MORPH_OPEN  # 模式选择-----> demo_erode, demo_dilate, MORPH_OPEN, MORPH_CLOSE, MORPH_GRADIENT, MORPH_TOPHAT, MORPH_BLACKHAT
+    input_dir = '/home/jerry/Desktop/garbage/111'  # 输入img和json // or only img // or img和部分json
+    filter = adaptive_brightness  # 模式选择-----> demo_erode, demo_dilate, MORPH_OPEN, MORPH_CLOSE, MORPH_GRADIENT, MORPH_TOPHAT, MORPH_BLACKHAT, adaptive_brightness
     probability = 1  # 0.8----->即随机处理80%的输入图像
     erode_img(input_dir, filter, probability, rename=False)  # Automatically create output folders under the same path as input_dir
